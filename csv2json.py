a76a1f19f97a0eba1b8459f75383699ad1ff0841
RubyBrewsday <poage.michael.cu@gmail.com>
2014-01-19 18:15:11 -0500

    Create csv2json.py
    Be sure to keep this in the same directory as 'scrape.py' when you run that program.

---
+++ csv2json.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+import json
+
+#EDIT THIS LIST WITH YOUR REQUIRED JSON KEY NAMES
+fieldnames=["title", "youTubeId"]
+
+def __unicode__(self):
+  return self.name
+
+def convert(filename):
+  csv_filename = str(filename.name)
+  print "Opening CSV file: ",csv_filename 
+  f=open(csv_filename, 'r+')
+  csv_reader = csv.DictReader(f,fieldnames)
+  json_filename = csv_filename.split(".")[0]+".json"
+  print "Saving JSON to file: ",json_filename
+  jsonf = open(json_filename,'w+') 
+  data = json.dumps([r for r in csv_reader])
+  jsonf.write(data) 
+  f.close()
+  jsonf.close()
+ 
+if __name__ == "__main__":
+  convert(sys.argv[1:])
